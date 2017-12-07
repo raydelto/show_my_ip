48184f3332147b008790a766c7cf030dcfdeb37a
Raydelto Hernandez <ray.hernandez@bluespurs.com>
2017-12-07 13:59:23 -0400

    Adding browser not supported message

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
+const ipDiv = document.getElementById('ip');
 const getIp = () => {
     fetch("http://ip.jsontest.com").then((response) => response.json())
     .then((response) => {
-        const ipDiv = document.getElementById('ip');
         ipDiv.innerHTML = `Your IP Address is ${response.ip}`;
     });
 }
 
-getIp();
+if(window.fetch){
+    getIp();
+}else{
+    const chromeURL = `https://www.google.ca/chrome/browser/desktop/index.html?brand=CHBD&gclid=CjwKCAiA6qPRBRAkEiwAGw4Sds1pEH-l4-GW-Pjths88f_Jef172SFuen7uAdueqXD1g8E8FaNlAWhoCYzcQAvD_BwE&gclsrc=aw.ds&dclid=CMzlxpe7-NcCFRGdyAodOWcHeg`;
+    ipDiv.innerHTML = `Your browser is not supported, please <a href="${chromeURL}">download another one</a>`;
+}
