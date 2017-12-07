cd4f632ff9c82f816329e143166997a67744ae40
Raydelto Hernandez <ray.hernandez@bluespurs.com>
2017-12-07 14:11:45 -0400

    Rewriting the code in ES5

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var ipDiv = document.getElementById('ip');
 if (window.fetch) {
-    fetch("http://ip.jsontest.com").then((response) => response.json())
-        .then((response) => {
-            ipDiv.innerHTML = `Your IP Address is ${response.ip}`;
+    fetch("http://ip.jsontest.com").then(function (response) {return response.json();})
+        .then(function(response) {
+            ipDiv.innerHTML = 'Your IP Address is ' + response.ip;
         });
 } else {
     var chromeURL = 'https://www.google.ca/chrome/browser/desktop/index.html?brand=CHBD&gclid=CjwKCAiA6qPRBRAkEiwAGw4Sds1pEH-l4-GW-Pjths88f_Jef172SFuen7uAdueqXD1g8E8FaNlAWhoCYzcQAvD_BwE&gclsrc=aw.ds&dclid=CMzlxpe7-NcCFRGdyAodOWcHeg';
