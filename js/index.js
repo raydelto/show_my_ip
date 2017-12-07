19564d77a1250a880a7aa13575ab19b3ee4da6d8
Raydelto Hernandez <ray.hernandez@bluespurs.com>
2017-12-07 14:07:15 -0400

    Making the code readable for older browsers

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const ipDiv = document.getElementById('ip');
+var ipDiv = document.getElementById('ip');
 if (window.fetch) {
     fetch("http://ip.jsontest.com").then((response) => response.json())
         .then((response) => {
             ipDiv.innerHTML = `Your IP Address is ${response.ip}`;
         });
 } else {
-    const chromeURL = `https://www.google.ca/chrome/browser/desktop/index.html?brand=CHBD&gclid=CjwKCAiA6qPRBRAkEiwAGw4Sds1pEH-l4-GW-Pjths88f_Jef172SFuen7uAdueqXD1g8E8FaNlAWhoCYzcQAvD_BwE&gclsrc=aw.ds&dclid=CMzlxpe7-NcCFRGdyAodOWcHeg`;
-    ipDiv.innerHTML = `Your browser is not supported, please <a href="${chromeURL}">download another one</a>`;
+    var chromeURL = 'https://www.google.ca/chrome/browser/desktop/index.html?brand=CHBD&gclid=CjwKCAiA6qPRBRAkEiwAGw4Sds1pEH-l4-GW-Pjths88f_Jef172SFuen7uAdueqXD1g8E8FaNlAWhoCYzcQAvD_BwE&gclsrc=aw.ds&dclid=CMzlxpe7-NcCFRGdyAodOWcHeg';
+    ipDiv.innerHTML = 'Your browser is not supported, please <a href="'+chromeURL+'" target="_blank">download another one</a>';
 }
