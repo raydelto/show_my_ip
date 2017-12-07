150fb8b0e11e0781d34610798b12234b9883896c
Raydelto Hernandez <ray.hernandez@bluespurs.com>
2017-12-07 14:02:42 -0400

    removing arrow functions

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 const ipDiv = document.getElementById('ip');
-const getIp = () => {
+if (window.fetch) {
     fetch("http://ip.jsontest.com").then((response) => response.json())
-    .then((response) => {
-        ipDiv.innerHTML = `Your IP Address is ${response.ip}`;
-    });
-}
-
-if(window.fetch){
-    getIp();
-}else{
+        .then((response) => {
+            ipDiv.innerHTML = `Your IP Address is ${response.ip}`;
+        });
+} else {
     const chromeURL = `https://www.google.ca/chrome/browser/desktop/index.html?brand=CHBD&gclid=CjwKCAiA6qPRBRAkEiwAGw4Sds1pEH-l4-GW-Pjths88f_Jef172SFuen7uAdueqXD1g8E8FaNlAWhoCYzcQAvD_BwE&gclsrc=aw.ds&dclid=CMzlxpe7-NcCFRGdyAodOWcHeg`;
     ipDiv.innerHTML = `Your browser is not supported, please <a href="${chromeURL}">download another one</a>`;
 }
